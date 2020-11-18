63d7a0e6840ecaaea084bebdbc2248823a4c640b
xanadu-zxl <xanadu.zxl@gamil.com>
2020-11-18 17:24:14 +0800

    feat：修改基础颜色

--- src/App.vue
+++ src/App.vue
@@ -21,20 +21,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #191f30;
   font-size: 14px;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
