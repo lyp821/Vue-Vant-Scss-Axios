fc130f19a8db50494bbbd2a1bc6320ff86beeda0
xanadu-zxl <xanadu.zxl@gamil.com>
2020-11-19 09:51:57 +0800

    feat：首页组件+轮播+下拉选择

--- src/main.js
+++ src/main.js
@@ -2,19 +2,43 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import global from './utils/global'
+import common from './utils/common'
 import './assets/icon/style.css'
-
 import 'lib-flexible'
-import { Tab, Tabs, Icon, Tabbar, TabbarItem, NavBar } from 'vant'
+import {
+  Tab,
+  Tabs,
+  Icon,
+  Tabbar,
+  TabbarItem,
+  NavBar,
+  DropdownMenu,
+  DropdownItem,
+  Button,
+  Swipe,
+  SwipeItem,
+  Lazyload,
+} from 'vant'
 
+Vue.use(Lazyload)
+Vue.use(Swipe)
+Vue.use(SwipeItem)
+Vue.use(Button)
+Vue.use(DropdownMenu)
+Vue.use(DropdownItem)
 Vue.use(Tab)
 Vue.use(Tabs)
 Vue.use(Icon)
 Vue.use(Tabbar)
 Vue.use(TabbarItem)
 Vue.use(NavBar)
+Vue.use(global)
+Vue.use(common)
 Vue.config.productionTip = false
 
+Vue.prototype.global = global
+Vue.prototype.common = common
 new Vue({
   router,
   store,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <van-nav-bar class="home-title" title="零工社区">
+    <van-nav-bar class="home-title" title="零工市场">
       <template #right>
         <van-icon name="search" size="18" />
       </template>
@@ -9,24 +9,45 @@
     <div class="content">
       <div class="left">
         <van-tabs v-model="active" animated>
-          <van-tab title="需求"> <div class="home-nav"></div> </van-tab>
-          <van-tab title="服务">123123</van-tab>
+          <van-tab title="需求" />
+          <van-tab title="服务" />
         </van-tabs>
+        <dropdown-item />
+        <van-swipe :autoplay="3000" height="120">
+          <van-swipe-item v-for="(image, index) in images" :key="index">
+            <img class="home-img" :src="image" alt="" />
+          </van-swipe-item>
+        </van-swipe>
+
+        <section class="section">
+          <img class="home-img_section" :src="sectionImgUrl" alt="" />
+        </section>
       </div>
 
       <div class="right">
-        <span class="location">成都</span>
+        <span class="location">{{ location }}</span>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import DropdownItem from '@/components/DropdownItem.vue'
+
 export default {
+  components: {
+    DropdownItem,
+  },
   data() {
     return {
       active: 0,
+      location: '未选择',
+      images: this.global.swipeImgs,
+      sectionImgUrl: this.global.sectionImgUrl,
     }
+  },
+  mounted() {
+    this.common.title('零工市场')
   },
 }
 </script>
