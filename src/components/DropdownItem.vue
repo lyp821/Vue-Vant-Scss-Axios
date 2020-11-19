296aac61d20556cefea7354b488e859aed9de95d
xanadu-zxl <xanadu.zxl@gamil.com>
2020-11-19 15:36:14 +0800

    feat：完成下拉刷新组件

--- src/components/DropdownItem.vue
+++ src/components/DropdownItem.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="dropdown">
-    <van-dropdown-menu>
+    <van-dropdown-menu active-color="#fe6b0d">
       <van-dropdown-item title="区域" v-model="value" :options="option" />
       <van-dropdown-item title="零工类型" v-model="value" :options="option" />
       <van-dropdown-item title="默认排序" v-model="value" :options="option" />
@@ -29,4 +29,32 @@ export default {
   },
 }
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss">
+.dropdown {
+  .van-dropdown-menu__bar {
+    box-shadow: none;
+    justify-content: space-between;
+    .van-dropdown-menu__item {
+      flex: none;
+      .van-ellipsis {
+        color: $Color;
+        font-size: 14px;
+      }
+      .van-dropdown-menu__title {
+        padding: 0 8px 0 0;
+      }
+      .van-dropdown-menu__title--active {
+        .van-ellipsis {
+          color: $themeColor;
+        }
+      }
+      .van-dropdown-menu__title::after {
+        border-color: transparent transparent $Color $Color;
+      }
+      .van-dropdown-menu__title--active::after {
+        border-color: transparent transparent $themeColor $themeColor;
+      }
+    }
+  }
+}
+</style>
