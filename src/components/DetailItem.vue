7b59a6c87405888067dacc78e6bf79eaa08931f0
xanadu-zxl <xanadu.zxl@gamil.com>
2020-11-20 14:44:31 +0800

    ✨ feat：列表展示组件完成

---
+++ src/components/DetailItem.vue
@@ -0,0 +1,133 @@
+<template>
+  <div class="detail">
+    <!-- {{ dataList }} -->
+    <div class="detail-item" v-for="item of dataList" :key="item.id">
+      <div class="detail-header">
+        <div class="detail-header-left">
+          <span class="detail-header_status">{{ item.status }}</span>
+          <span class="detail-header_title">{{ item.title }}</span>
+          <span class="detail-header_grade">诚信值:{{ item.grade }}</span>
+        </div>
+        <span class="detail-header_price">{{ item.money }}/天</span>
+      </div>
+
+      <p class="detail-address">{{ item.address }}</p>
+      <p class="detail-category">
+        <span v-for="category in item.category" :key="category.id">{{ category.title }}</span>
+      </p>
+      <div class="detail-company">
+        <img :src="item.logo" alt="" />
+        <span>{{ item.company }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['dataList'],
+  data() {
+    return {}
+  },
+  mounted() {
+    console.log(this.dataList)
+  },
+}
+</script>
+<style lang="scss">
+.detail {
+  margin-bottom: 60px;
+  .detail-item {
+    padding: 14px 0px;
+  }
+  .detail-header {
+    height: 22px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    .detail-header-left {
+      display: flex;
+      align-items: center;
+
+      .detail-header_status {
+        display: inline-block;
+        width: 30px;
+        border-radius: 2px;
+        padding: 0 2px;
+        background: $themeColor;
+        font-size: 11px;
+        color: #fff;
+      }
+
+      .detail-header_title {
+        display: inline-block;
+        height: 22px;
+        padding: 0 8px;
+        font-size: 16px;
+        font-weight: 600;
+        line-height: 22px;
+      }
+
+      .detail-header_grade {
+        font-size: 11px;
+        font-weight: 400;
+        padding: 0 2px;
+        color: rgba(67, 188, 8, 1);
+        border: 1px solid rgba(67, 188, 8, 1);
+        border-radius: 2px;
+      }
+    }
+    .detail-header_price {
+      @extend .detail-header_title;
+      color: $themeColor;
+      font-weight: 600;
+    }
+  }
+
+  .detail-address {
+    margin: 4px 0 8px;
+    font-size: 13px;
+    line-height: 17px;
+    color: #95a4b3;
+    text-align: left;
+    @include ell;
+  }
+
+  .detail-category {
+    text-align: left;
+
+    span {
+      font-size: 11px;
+      line-height: 16px;
+      text-align: center;
+      color: $Color;
+      padding: 2px 4px;
+      background: #f4f4f8;
+      border-radius: 2px;
+      margin-right: 4px;
+    }
+  }
+
+  .detail-company {
+    margin-top: 12px;
+    display: flex;
+    justify-content: start;
+    align-items: center;
+    img {
+      display: block;
+      width: 20px;
+      height: 20px;
+      border: 0.5px solid #e9eaeb;
+      box-sizing: border-box;
+      border-radius: 20px;
+    }
+    span {
+      margin-left: 4px;
+      text-align: center;
+      color: $Color;
+      font-size: 14px;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,30 +12,35 @@
           <van-tab title="需求" />
           <van-tab title="服务" />
         </van-tabs>
-        <dropdown-item />
-        <van-swipe :autoplay="3000" height="120">
-          <van-swipe-item v-for="(image, index) in images" :key="index">
-            <img class="home-img" :src="image" alt="" />
-          </van-swipe-item>
-        </van-swipe>
-
-        <section class="section">
-          <img class="home-img_section" :src="sectionImgUrl" alt="" />
-        </section>
       </div>
-
       <div class="right">
         <span class="location">{{ location }}</span>
       </div>
     </div>
+    <main class="home-main">
+      <dropdown-item />
+      <van-swipe :autoplay="3000" height="120">
+        <van-swipe-item v-for="(image, index) in images" :key="index">
+          <img class="home-img" :src="image" alt="" />
+        </van-swipe-item>
+      </van-swipe>
+
+      <section class="home-section">
+        <p class="home-p">公共资源服务</p>
+      </section>
+
+      <detail-item :dataList="dataList"></detail-item>
+    </main>
   </div>
 </template>
 
 <script>
 import DropdownItem from '@/components/DropdownItem.vue'
+import DetailItem from '@/components/DetailItem.vue'
 
 export default {
   components: {
+    DetailItem,
     DropdownItem,
   },
   data() {
@@ -44,6 +49,52 @@ export default {
       location: '未选择',
       images: this.global.swipeImgs,
       sectionImgUrl: this.global.sectionImgUrl,
+      dataList: [
+        {
+          id: 1,
+          status: '需求',
+          title: '后厨墩子',
+          grade: '100',
+          money: 200,
+          address: '四川省成都市青羊区大石西路56号',
+          category: [{ title: '计时' }, { title: '上门服务' }, { title: '在线零工' }],
+          logo: require('../assets/img/公共资源服务.png'),
+          company: '潘晓明',
+        },
+        {
+          id: 2,
+          status: '需求',
+          title: '兼职经纪人',
+          grade: '100',
+          money: 200,
+          address: '青羊区光华大道一段866号(西三环成温大道与武青路交叉口',
+          category: [{ title: '计时' }, { title: '上门服务' }, { title: '在线零工' }],
+          logo: require('../assets/img/公共资源服务.png'),
+          company: '成都科技有限公司',
+        },
+        {
+          id: 3,
+          status: '需求',
+          title: '兼职经纪人',
+          grade: '100',
+          money: 200,
+          address: '青羊区光华大道一段866号(西三环成温大道与武青路交叉口',
+          category: [{ title: '计时' }, { title: '上门服务' }, { title: '在线零工' }],
+          logo: require('../assets/img/公共资源服务.png'),
+          company: '成都科技有限公司',
+        },
+        {
+          id: 4,
+          status: '需求',
+          title: '兼职经纪人',
+          grade: '100',
+          money: 200,
+          address: '青羊区光华大道一段866号(西三环成温大道与武青路交叉口',
+          category: [{ title: '计时' }, { title: '上门服务' }, { title: '在线零工' }],
+          logo: require('../assets/img/公共资源服务.png'),
+          company: '成都科技有限公司',
+        },
+      ],
     }
   },
   mounted() {
